adfbd78604528e7dfc8b651bd36ac8c604a6f325
arielvergarautn <arie@est.frba.utn.edu.ar>
2020-10-23 21:14:54 -0300

    Tests

---
+++ Backend/TravelLine/TravelLine.IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelLine.IntegrationTest
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient _TestClient;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                                .WithWebHostBuilder(builder => builder.ConfigureServices(services => {
+                                    services.RemoveAll(typeof(TravelLine.DAOs.TravelLineContext));
+                                    services.AddDbContext<TravelLine.DAOs.TravelLineContext>(options => {
+                                        options.UseInMemoryDatabase("TestDb");
+                                    });
+                                })
+                            );
+
+            _TestClient = appFactory.CreateClient();
+        }
+
+
+        //protected async Task AuthenticateAsync()
+        //{
+
+        //}
+
+
+    }
+}
---
+++ Backend/TravelLine/TravelLine.IntegrationTest/PostControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TravelLine.IntegrationTest
+{
+    public class PostControllerTest : IntegrationTest
+    {
+        [Fact]
+        public async Task Task_GetAll_WithoutAnyPost_ReturnEmptyResponse()
+        {
+            //Arrange
+
+            //Act
+            var res = await _TestClient.GetAsync("https://localhost:44342/api/posts");
+            //Assert
+            res.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await res.Content.ReadAsStringAsync()).Should().BeEmpty();
+        }
+    }
+}
